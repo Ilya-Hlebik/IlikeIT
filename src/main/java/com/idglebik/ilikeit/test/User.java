544f951b51ff5619b302997d6b63259b3b6e5049
TalkmeProject <felix.2894@mail.ru>
2018-09-09 19:58:24 +0300

    update 0.2.3 - security

---
+++ src/main/java/com/idglebik/ilikeit/test/User.java
@@ -0,0 +1,40 @@
+package com.idglebik.ilikeit.test;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Size(min = 4, max = 255, message = "Minimum username length: 4 characters")
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Size(min = 8, message = "Minimum password length: 8 characters")
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @EqualsAndHashCode.Exclude
+    List<Role> roles;
+
+
+}
