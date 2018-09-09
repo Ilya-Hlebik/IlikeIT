45bb1a755c9c0f5bde2f626f9e61ecbc07bfe1d9
TalkmeProject <felix.2894@mail.ru>
2018-09-09 19:57:08 +0300

    update 0.2.2 - some refactoring-

--- src/main/java/com/idglebik/ilikeit/ProjectStartDemoApplication.java
+++ src/main/java/com/idglebik/ilikeit/ProjectStartDemoApplication.java
@@ -2,9 +2,16 @@ package com.idglebik.ilikeit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ProjectStartDemoApplication {
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectStartDemoApplication.class, args);
